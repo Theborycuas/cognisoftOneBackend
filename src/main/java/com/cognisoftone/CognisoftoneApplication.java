a2e04a5183f99c5fce93cd7181a67317f46b3663
Borys <borysjair96@gmail.com>
2025-07-20 15:07:37 -0500

    Start init project Cognisoft-One

---
+++ src/main/java/com/cognisoftone/CognisoftoneApplication.java
@@ -0,0 +1,13 @@
+package com.cognisoftone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognisoftoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CognisoftoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cognisoftone/CognisoftoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognisoftone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CognisoftoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
